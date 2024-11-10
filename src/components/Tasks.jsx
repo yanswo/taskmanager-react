3ccc4c63b47d5f4716a663ae5307836e92bb6c8b
Yan Lucas <88909517+yanswo@users.noreply.github.com>
2024-11-10 00:55:05 -0300

    Corrigindo bug de não dar concluido na tarefa.

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -17,10 +17,9 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
-              task.isCompleted && "line-through"
-            }`}
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md`}
           >
+            {task.isCompleted && <CheckIcon />
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
